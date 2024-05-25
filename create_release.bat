8b11ea999cac622113d139f1827ee30f8c88eb55
程捷 <1426163582@qq.com>
2024-05-25 15:45:46 +0800

    test

--- create_release.bat
+++ create_release.bat
@@ -1,2 +1,11 @@
-cd "D:\Steam\steamapps\common\Don't Starve Together\mods\gale"
-git clone ./ ../gale_release
+@echo off
+
+set DST_MOD_DIR=D:\Steam\steamapps\common\Don't Starve Together\mods
+set GALE_MOD_DIR=%DST_MOD_DIR%\gale
+set RELEASE_MOD_DIR=%DST_MOD_DIR%\gale_release
+
+@REM cd "%GALE_MOD_DIR%"
+git clone "%GALE_MOD_DIR%" "%RELEASE_MOD_DIR%" 
+
+@REM cd %RELEASE_MOD_DIR%
+@REM rm -rf .git
